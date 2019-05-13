e51908ad3d0c386572e300dc01f73dc18b1143eb
dindavirguinia <36815554+dindavirguinia@users.noreply.github.com>
2019-05-13 11:15:09 +0700

    Update Jadwal.java

--- app/src/main/java/com/example/lenovo/aps1/Jadwal.java
+++ app/src/main/java/com/example/lenovo/aps1/Jadwal.java
@@ -1,13 +1,134 @@
 package com.example.lenovo.aps1;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.AlarmManager;
 
-public class Jadwal extends AppCompatActivity {
+import android.app.PendingIntent;
+
+import android.content.Intent;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.TimePicker;
+
+import android.widget.Toast;
+
+
+import java.util.Calendar;
+
+
+
+
+public class Jadwal extends AppCompatActivity implements View.OnClickListener{
+
+
+
+    private int notificationId = 1;
+
+
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jadwal);
+
+// Set Onclick Listener.
+        Button aturBtn = (Button) findViewById(R.id.setBtn);
+        Button tolakBtn = (Button) findViewById(R.id.cancelBtn);
+
+        findViewById(R.id.setBtn).setOnClickListener(this);
+
+        findViewById(R.id.cancelBtn).setOnClickListener(this);
+
+    }
+
+    @Override
+
+    public void onClick(View view) {
+
+        EditText editText = findViewById(R.id.inputdata);
+
+        TimePicker timePicker = findViewById(R.id.pilihwaktu);
+
+
+
+// Set notificationId & text.
+
+        Intent intent = new Intent(Jadwal.this, AlarmReceiver.class);
+
+        intent.putExtra("notificationId", notificationId);
+
+        intent.putExtra("todo", editText.getText().toString());
+
+
+
+// getBroadcast(context, requestCode, intent, flags)
+
+
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(Jadwal.this, 0,
+                intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+
+        AlarmManager alarm = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+
+
+        switch (view.getId()) {
+
+            case R.id.setBtn:
+
+                int hour = timePicker.getCurrentHour();
+
+                int minute = timePicker.getCurrentMinute();
+
+
+
+
+// Create time.
+
+                Calendar startTime = Calendar.getInstance();
+
+                startTime.set(Calendar.HOUR_OF_DAY, hour);
+
+                startTime.set(Calendar.MINUTE, minute);
+
+                startTime.set(Calendar.SECOND, 0);
+
+                long alarmStartTime = startTime.getTimeInMillis();
+
+
+
+// Set alarm.
+
+// set(type, milliseconds, intent)
+
+                alarm.set(AlarmManager.RTC_WAKEUP, alarmStartTime, alarmIntent);
+
+
+
+                Toast.makeText(this, "Done!", Toast.LENGTH_SHORT).show();
+
+                break;
+
+
+
+            case R.id.cancelBtn:
+
+                alarm.cancel(alarmIntent);
+
+                Toast.makeText(this, "Canceled.", Toast.LENGTH_SHORT).show();
+
+                break;
+
+        }
+
+
     }
 }
